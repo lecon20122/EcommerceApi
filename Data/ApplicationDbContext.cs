d6689c94ffca81075cfe8225b989120a88a4a642
lecon20122 <lecon20122@gmail.com>
2023-07-17 16:01:41 +0300

    Creating initial Migration and db update , setup the identity Models and Context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
